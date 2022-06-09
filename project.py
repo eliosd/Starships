895988852cfd9cc0629901b2ecd2999006e3772d
konrad1208 <32593523+konrad1208@users.noreply.github.com>
2022-06-09 17:06:10 +0100

    Refactored the largest function, and simplified other functions

--- project.py
+++ project.py
@@ -3,45 +3,37 @@ import pymongo
 from tqdm import tqdm
 
 # configure database access
-client = pymongo.MongoClient()
-db = client["starwars"]
-stars = db["starship"]
-characters = db["characters"]
+db = pymongo.MongoClient()["starwars"]
+starships, characters = db["starship"], db["characters"]
 
 
-def extract_api_data(url):
-    get_request = requests.get(url)
-    data = get_request.json()
-    return data
+def get_api_data(url, key):
+    data = requests.get(url).json()
+    return data[key]
 
 
 def change_pilot_links_to_names(data):
-    new_stars = []
-    name_2_id = generate_name_id()
+    new_starships = []
     link_2_id = {}
 
-    for index, star_obj in enumerate(tqdm(data["results"])):
-        for pilot_link in star_obj["pilots"]:
-            if pilot_link not in link_2_id:
-                name = pilot_link_to_name(pilot_link)
-                link_2_id[pilot_link] = name_2_id[name]
-
-        # change pilot links to id, hence add to list
-        star_obj = map_pilot_link_2_id(star_obj, link_2_id)
-        new_stars.append(star_obj)
-    return new_stars
+    # update pilot/id translations, hence modify starship to use name
+    for i, starship in enumerate(tqdm(data)):
+        link_2_id = update_dictionary(starship, link_2_id)
+        starship = map_pilot_link_2_id(starship, link_2_id)
+        new_starships.append(starship)
+    return new_starships
 
 
-def pilot_link_to_name(link):
-    data = extract_api_data(link)
-    return data["name"]
+def update_dictionary(starship, link_2_id):
+    for pilot_link in starship["pilots"]:
+        if pilot_link not in link_2_id:
+            name = get_api_data(pilot_link, "name")
+            link_2_id[pilot_link] = get_id_of_name(name)
+    return link_2_id
 
 
-def generate_name_id():
-    translation = {}
-    for c in characters.find():
-        translation[c["name"]] = c["_id"]
-    return translation
+def get_id_of_name(n):
+    return characters.find_one({"name": n}, {"_id": 1})["_id"]
 
 
 def map_pilot_link_2_id(star_list, link_to_name):
@@ -51,24 +43,20 @@ def map_pilot_link_2_id(star_list, link_to_name):
     return star_list
 
 
-def view_data(data, options=None):
-    if options:
-        for i, v in enumerate(data):
-            for opt in options:
-                print(f"{opt:<7}= {v[opt]}")
-            print("\n", end="")
-    else:
-        for i in data:
-            print(i, end="\n\n")
+def view_data(data, options):
+    for i, v in enumerate(data):
+        for opt in options:
+            print(f"{opt:<7}= {v[opt]}")
+        print("\n", end="")
 
 
 # MAIN
 
-starship_api_data = extract_api_data("https://swapi.dev/api/starships")
-starship_list = change_pilot_links_to_names(starship_api_data)
+api_date = get_api_data("https://swapi.dev/api/starships", "results")
+starship_list = change_pilot_links_to_names(api_date)
 
 # Clean the starship database first, then insert
-stars.drop()
-stars.insert_many(starship_list)
+starships.drop()
+starships.insert_many(starship_list)
 
 view_data(starship_list, options=["name", "pilots"])

b777cda5315eb436e32519e946a1841b5ea5b47f
konrad1208 <32593523+konrad1208@users.noreply.github.com>
2022-06-09 15:28:44 +0100

    complete refactoring into functions

--- project.py
+++ project.py
@@ -1,92 +1,67 @@
 import requests
 import pymongo
 
+# configure database access
 client = pymongo.MongoClient()
 db = client["starwars"]
 stars = db["starship"]
 characters = db["characters"]
 
-x = requests.get("https://swapi.dev/api/starships")
 
-# x = requests.post(
-#     url="https://api/starships"
-#     headers=headers,
-#     data=json.dumps(body)
-#     )
+def get_api_data(url):
+    get_request = requests.get(url)
+    data = get_request.json()
+    return data
 
-pcs = x.json()
 
-# print(len(pcs["results"][0]))
-print(pcs["results"][0])
+def extract_from_api_date(data):
+    return_list = []
+    translation = {}
+    for index, starship_item in enumerate(data["results"]):
+        for pilot_link in starship_item["pilots"]:
+            translation[pilot_link] = pilot_url_to_name(pilot_link)
+        return_list.append(starship_item)
+    return return_list, translation
 
-pilot_list = []
-starship_list = []
-comb = []
-raw = []
-translate = {}
 
-for i in range(len(pcs["results"]) - 1):
-    print(i, pcs["results"][i]["pilots"])
-    for j in pcs["results"][i]["pilots"]:
-        pilot_list.append(j)
-        starship_list.append(pcs["results"][i]["name"])
-    raw.append(pcs["results"][i])
+def pilot_url_to_name(link):
+    data = get_api_data(link)
+    return data["name"]
 
-# print(len(pilot_list), pilot_list, sep="\n")
 
-for i, v in enumerate(pilot_list):
-    x = requests.get(v)
-    pcs = x.json()
-    print(starship_list[i], pcs["name"], sep="\n", end="\n\n")
-    translate[v] = pcs["name"]
+def generate_name_id():
+    translation = {}
+    for c in characters.find():
+        translation[c["name"]] = c["_id"]
+    return translation
 
-a = db.starship.find()
 
-# for i in raw:
-#     print(i, sep="\n\n")
+def starship_pilot_to_id(star_list, link_to_name, name_to_id):
+    for index_star, starship in enumerate(star_list):
+        for index_pilot, pilot_link in enumerate(starship["pilots"]):
+            pilot_name = link_to_name[pilot_link]
+            pilot_id = name_to_id[pilot_name]
+            star_list[index_star]["pilots"][index_pilot] = pilot_id
+    return star_list
 
-translate2 = {}
-for c in characters.find():
-    # print(c["name"], c["_id"])
-    translate2[c["name"]] = c["_id"]
-print(translate2)
 
-for i, v in enumerate(raw):
-    for j, v2 in enumerate(raw[i]["pilots"]):
-        try:
-            raw[i]["pilots"][j] = translate[raw[i]["pilots"][j]]
-        except:
-            print("f")
-            pass
-        # print(raw[i]["pilots"])
-        # print(raw[i]["pilots"][j], translate[raw[i]["pilots"][j]])
-    # print(i, v, translate[raw[0]["pilots"][0]], sep="\n", end="\n\n")
-# print(translate[raw[0]["pilots"][0]])
+def view_data(data):
+    for i in data:
+        print(i, end="\n\n")
 
-# print(raw[0]["pilots"])
-# print(translate)
-# for i in raw:
-#     print(i["pilots"])
 
-# for i in raw:
-#     print(i, end="\n\n")
+# MAIN
 
-# print(translate)
+starship_api_data = get_api_data("https://swapi.dev/api/starships")
+starship_list, link_2_name = extract_from_api_date(starship_api_data)
+name_2_id = generate_name_id()
+starship_list = starship_pilot_to_id(starship_list, link_2_name, name_2_id)
 
-# for i in x.text:
-#     print(i, end="")
 
-# for i, v in enumerate(raw):
-#     raw[i]["_id"] = i
-# for i in raw:
-#     print(i, end="\n\n")
-
-# for j in raw:
+# Clean the starship database first, then insert
 stars.drop()
-stars.insert_many(raw)
+stars.insert_many(starship_list)
 
-# print(type(raw), type(raw[0]))
-# for i in raw:
-#     print(i, sep="\n\n")
 
+view_data(starship_list)
 

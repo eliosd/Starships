9dda93218288a8c5fd2574c459fb6ec57ed08b26
konrad1208 <32593523+konrad1208@users.noreply.github.com>
2022-06-09 17:33:11 +0100

    Removed unnecessary steps in the core function in main

--- project.py
+++ project.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 # configure database access
 db = pymongo.MongoClient()["starwars"]
 starships, characters = db["starship"], db["characters"]
+link_2_id = {}
 
 
 def get_api_data(url, key):
@@ -12,19 +13,22 @@ def get_api_data(url, key):
     return data[key]
 
 
-def change_pilot_links_to_names(data):
+def change_starship_pilot_link_to_id(data):
     new_starships = []
-    link_2_id = {}
-
-    # update pilot/id translations, hence modify starship to use name
-    for i, starship in enumerate(tqdm(data)):
-        link_2_id = update_dictionary(starship, link_2_id)
-        starship = map_pilot_link_2_id(starship, link_2_id)
-        new_starships.append(starship)
+    for starship in tqdm(data):
+        new_starships.append(pilot_link_to_id(starship))
     return new_starships
 
 
-def update_dictionary(starship, link_2_id):
+def pilot_link_to_id(starship):
+    link_to_id = update_dictionary(starship)
+    for index, pilot_link in enumerate(starship["pilots"]):
+        starship["pilots"][index] = link_to_id[pilot_link]
+    return starship
+
+
+def update_dictionary(starship):
+    global link_2_id
     for pilot_link in starship["pilots"]:
         if pilot_link not in link_2_id:
             name = get_api_data(pilot_link, "name")
@@ -34,13 +38,6 @@ def update_dictionary(starship, link_2_id):
 
 def get_id_of_name(n):
     return characters.find_one({"name": n}, {"_id": 1})["_id"]
-
-
-def map_pilot_link_2_id(star_list, link_to_name):
-    for index_pilot, pilot_link in enumerate(star_list["pilots"]):
-        pilot_id = link_to_name[pilot_link]
-        star_list["pilots"][index_pilot] = pilot_id
-    return star_list
 
 
 def view_data(data, options):
@@ -53,7 +50,7 @@ def view_data(data, options):
 # MAIN
 
 api_data = get_api_data("https://swapi.dev/api/starships", "results")
-starship_list = change_pilot_links_to_names(api_data)
+starship_list = change_starship_pilot_link_to_id(api_data)
 
 # Clean the starship database first, then insert
 starships.drop()
